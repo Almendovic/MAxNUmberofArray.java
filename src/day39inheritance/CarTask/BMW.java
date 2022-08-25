6656064d2a0e919ab1af865bb723d1f484755c10
Alen <almendovic@gmail.com>
2022-08-25 16:41:49 -0600

    javaB27

---
+++ src/day39inheritance/CarTask/BMW.java
@@ -0,0 +1,17 @@
+package day39inheritance.CarTask;
+
+public class BMW extends Car{
+
+
+    public BMW( String model, int year, double price, String color, int miles) {
+        super("BMW", model, year, price, color, miles);
+    }
+
+    public void breaksDown(){
+        System.out.println(brand+" "+model+" is breaking down");
+    }
+
+
+
+
+}
