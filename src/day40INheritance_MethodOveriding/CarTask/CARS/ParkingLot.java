11ca0263ab2bf9e880bdf911ef2ac51ec8a307bd
Alen <almendovic@gmail.com>
2022-08-25 17:05:02 -0600

    javaB27

---
+++ src/day40INheritance_MethodOveriding/CarTask/CARS/ParkingLot.java
@@ -0,0 +1,15 @@
+package day40INheritance_MethodOveriding.CarTask.CARS;
+
+public class ParkingLot {//todo parking lot has a relation
+    public static void main(String[] args) {
+
+
+        Toyota toyota =new Toyota("Camry",2019,18000,"Gray",123456);
+
+        Tesla tesla=new Tesla("Model S",2022,94990,"Red",0);
+
+        BMW bmw=new BMW("X5",2020,67350,"Black",120000);
+
+
+    }
+}
